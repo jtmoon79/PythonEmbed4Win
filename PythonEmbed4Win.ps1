d8289051482f415620ed620ae7938385f3564179
James Thomas Moon (@jtmoon79) <815261+jtmoon79@users.noreply.github.com>
2023-11-07 22:18:27 -0800

    sitecustomize.py also remove USER_SITE realpath

--- PythonEmbed4Win.ps1
+++ PythonEmbed4Win.ps1
@@ -711,13 +711,32 @@ import site".Replace("`r`n", "`n")
     # 3. set sitecustomize.py
     $python_site_path = Join-Path -Path "." -ChildPath "sitecustomize.py"
     $content_sitecustomize = "# sitecustomize.py
-import sys
+#
+# this file was added by PythonEmbed4Win.ps1
+
+import os
 import site
-# do not use user-wide site.USER_SITE path, it refers to location outside this embed installation
-# this hack was added by ${SCRIPT_NAME}
+import sys
+
+# do not use user-wide site.USER_SITE path; it refers to a path location
+# outside of this embed installation
 site.ENABLE_USER_SITE = False
-if site.USER_SITE in sys.path:
-    sys.path.remove(site.USER_SITE)".Replace("`r`n", "`n")
+
+# remove site.USER_SITE and the realpath variation from sys.path
+__sys_path_index_del = list()
+`"`"`"index to delete from sys.path`"`"`"
+__user_site_resolve = os.path.realpath(site.USER_SITE)
+for __i, __path in enumerate(sys.path):
+    __path_resolve = os.path.realpath(__path)
+    if site.USER_SITE in (__path, __path_resolve):
+        __sys_path_index_del.append(__i)
+        continue
+    if __user_site_resolve in (__path, __path_resolve):
+        __sys_path_index_del.append(__i)
+for __index_del in reversed(__sys_path_index_del):
+    sys.path.pop(__index_del)
+del __sys_path_index_del
+".Replace("`r`n", "`n")
     # use 'ascii' encoding, see above
     $content_sitecustomize | Out-File -FilePath $python_site_path -Encoding "ascii"
     Print-File-Nicely $python_site_path
