e208cfb4085c630dfac3e6b9be5f47a511edc3e0
James Thomas Moon (@jtmoon79) <815261+jtmoon79@users.noreply.github.com>
2023-12-31 14:07:13 -0800

    NFC code comment time-consuming path check

--- PythonEmbed4Win.ps1
+++ PythonEmbed4Win.ps1
@@ -748,6 +748,7 @@ import sys
 site.ENABLE_USER_SITE = False
 
 # remove site.USER_SITE and the realpath variation from sys.path
+# XXX: somewhat time consuming to do on every startup but thorough
 __sys_path_index_del = list()
 `"`"`"index to delete from sys.path`"`"`"
 __user_site_resolve = os.path.realpath(site.USER_SITE)
